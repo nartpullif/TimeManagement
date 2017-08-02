05b2c21474dce16c124ecd01049aa293a55c65d1
icyfillup <phil10tran@gmail.com>
2017-08-01 19:20:26 -0700

    creating sharepreference across the activities

---
+++ app/src/main/java/com/example/android/timemanagement/utilities/PreferenceUtils.java
@@ -0,0 +1,13 @@
+package com.example.android.timemanagement.utilities;
+
+/**
+ * Created by icyfillup on 8/1/2017.
+ */
+
+public class PreferenceUtils
+{
+    public static void initSharePreference()
+    {
+
+    }
+}
