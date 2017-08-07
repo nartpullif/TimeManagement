fdbd8b33ffe900fbc80e5c8507d6364f14ea1de2
icyfillup <phil10tran@gmail.com>
2017-08-07 15:29:08 -0700

    notification pop up can now redirect to HomeTabActivity when click

--- app/src/main/java/com/example/android/timemanagement/notification/Notification_reciever.java
+++ app/src/main/java/com/example/android/timemanagement/notification/Notification_reciever.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
+import com.example.android.timemanagement.activities.HomeTabActivity;
 import com.example.android.timemanagement.activities.MainActivity;
 
 /**
@@ -19,7 +20,7 @@ public class Notification_reciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent){
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent back_intent  =new Intent(context, MainActivity.class);
+        Intent back_intent  =new Intent(context, HomeTabActivity.class);
         back_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,100,back_intent,PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
